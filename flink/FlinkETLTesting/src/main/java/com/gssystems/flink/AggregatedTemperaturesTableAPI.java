f3d74d8fe41393bf09143bfc2f7dfdb4ebb54bab
Venkata Jagannath <venky_jagannath@hotmail.com>
2023-09-24 18:53:24 -0500

    test

--- flink/FlinkETLTesting/src/main/java/com/gssystems/flink/AggregatedTemperaturesTableAPI.java
+++ flink/FlinkETLTesting/src/main/java/com/gssystems/flink/AggregatedTemperaturesTableAPI.java
@@ -2,6 +2,7 @@ package com.gssystems.flink;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
@@ -12,6 +13,8 @@ import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
+
+import com.google.gson.Gson;
 
 public class AggregatedTemperaturesTableAPI {
 	
@@ -25,9 +28,22 @@ public class AggregatedTemperaturesTableAPI {
 		// Build input stream
 		final FileSource<String> source = FileSource
 				.forRecordStreamFormat(new TextLineInputFormat(), new Path(params.get("input"))).build();
-
 		final DataStream<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "file-source");
-		Table table1 = tableEnv.fromDataStream(stream);
+		
+		MapFunction<String, TemperatureAggregateBean> x1 = new MapFunction<String, TemperatureAggregateBean>() {
+			private static final long serialVersionUID = -1385202325062265709L;
+
+			@Override
+			public TemperatureAggregateBean map(String value) throws Exception {
+				Gson gs = new Gson();
+				TemperatureAggregateBean aBean = gs.fromJson(value, TemperatureAggregateBean.class);
+				return aBean;
+			}
+		};
+		//Let us convert the JSON Stream into a stream of java objects.
+		final DataStream<TemperatureAggregateBean> pojoStream = stream.map(x1 );
+		
+		Table table1 = tableEnv.fromDataStream(pojoStream);
 		System.out.println("Printing the table from stream...");
 		table1.execute().print();
 		
