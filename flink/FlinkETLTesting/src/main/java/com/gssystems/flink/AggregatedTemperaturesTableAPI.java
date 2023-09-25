ceb24ebeb4712f66908945a12536b36eef230951
Venkata Jagannath <venky_jagannath@hotmail.com>
2023-09-25 08:36:11 -0500

    working table api json parsing

--- flink/FlinkETLTesting/src/main/java/com/gssystems/flink/AggregatedTemperaturesTableAPI.java
+++ flink/FlinkETLTesting/src/main/java/com/gssystems/flink/AggregatedTemperaturesTableAPI.java
@@ -2,7 +2,6 @@ package com.gssystems.flink;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
@@ -10,16 +9,17 @@ import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.expressions.Expression;
 import org.apache.flink.table.expressions.SqlCallExpression;
 import org.apache.flink.types.Row;
-import org.apache.flink.util.CloseableIterator;
 
-import com.google.gson.Gson;
-
+/**
+ * This program will take the aggregated JSONs we produced before, and use Table
+ * API to parse and create a table that we can interact with way easily than
+ * traditional java programming
+ */
 public class AggregatedTemperaturesTableAPI {
 
 	public static void main(String[] args) throws Exception {
@@ -33,67 +33,19 @@ public class AggregatedTemperaturesTableAPI {
 				.forRecordStreamFormat(new TextLineInputFormat(), new Path(params.get("input"))).build();
 		final DataStream<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "file-source");
 
-		MapFunction<String, Row> x1 = new MapFunction<String, Row>() {
-			private static final long serialVersionUID = -1385202325062265709L;
-
-			@Override
-			public Row map(String value) throws Exception {
-				Gson gs = new Gson();
-				TemperatureAggregateBean aBean = gs.fromJson(value, TemperatureAggregateBean.class);
-				Row aRow = Row.of(aBean.getLat(), aBean.getLng(), aBean.getYear(), aBean.getMonth(), aBean.getCount(),
-						aBean.getMinTemp(), aBean.getMaxTemp());
-				return aRow;
-			}
-		};
-		
-		// Let us convert the JSON Stream into a stream of Row objects.
-		final DataStream<Row> pojoStream = stream.map(x1);
-		
-		Schema tableSchema = Schema.newBuilder()
-				.column("latitude", DataTypes.DOUBLE())
-				.column("longitude", DataTypes.DOUBLE())
-				.column("year", DataTypes.INT())
-				.column("month", DataTypes.INT())
-				.column("count", DataTypes.DOUBLE())
-				.column("MinTemp", DataTypes.DOUBLE())
-				.column("MaxTemp", DataTypes.DOUBLE())
-				.build();
-		
-		CloseableIterator<Row> iter = pojoStream.executeAndCollect();
-		while(iter.hasNext()) {
-			Row nextRow = iter.next();
-			System.out.println(nextRow);
-		}
-
-		
 		StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
 		Table table1 = tableEnv.fromDataStream(stream);
 		System.out.println("Printing the table from stream...");
-		Expression selExpr1 = new SqlCallExpression(
-				"JSON_VALUE(f0, '$.lat')"
-		);
-		Expression selExpr2 = new SqlCallExpression(
-				"JSON_VALUE(f0, '$.lng')"
-		);
-		Expression selExpr3 = new SqlCallExpression(
-				"JSON_VALUE(f0, '$.year')"
-		);
-		Expression selExpr4 = new SqlCallExpression(
-				"JSON_VALUE(f0, '$.month')"
-		);
-		Expression selExpr5 = new SqlCallExpression(
-				"JSON_VALUE(f0, '$.count')"
-		);
-		Expression selExpr6 = new SqlCallExpression(
-				"JSON_VALUE(f0, '$.minTemp')"
-		);
-		Expression selExpr7 = new SqlCallExpression(
-				"JSON_VALUE(f0, '$.maxTemp')"
-		);
-		table1.select(selExpr1,selExpr2, selExpr3, selExpr4, selExpr5, selExpr6, selExpr7 )
-		.as("longitude","year","month","count","min_temp","max_temp")		
-		.execute().print();
+		Expression selExpr1 = new SqlCallExpression("JSON_VALUE(f0, '$.lat')");
+		Expression selExpr2 = new SqlCallExpression("JSON_VALUE(f0, '$.lng')");
+		Expression selExpr3 = new SqlCallExpression("JSON_VALUE(f0, '$.year')");
+		Expression selExpr4 = new SqlCallExpression("JSON_VALUE(f0, '$.month')");
+		Expression selExpr5 = new SqlCallExpression("JSON_VALUE(f0, '$.count')");
+		Expression selExpr6 = new SqlCallExpression("JSON_VALUE(f0, '$.minTemp')");
+		Expression selExpr7 = new SqlCallExpression("JSON_VALUE(f0, '$.maxTemp')");
+		table1.select(selExpr1, selExpr2, selExpr3, selExpr4, selExpr5, selExpr6, selExpr7)
+				.as("longitude", "year", "month", "count", "min_temp", "max_temp").execute().print();
 		table1.printSchema();
 
 		Table inputTable = tableEnv.fromValues(DataTypes.ROW(DataTypes.FIELD("id", DataTypes.DECIMAL(10, 2)),
