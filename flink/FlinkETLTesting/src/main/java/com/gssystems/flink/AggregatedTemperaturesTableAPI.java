1a896d0cbc3f1fefeb6f72f6977369d3bb16cf99
Venkata Jagannath <venky_jagannath@hotmail.com>
2023-09-25 07:28:25 -0500

    test

--- flink/FlinkETLTesting/src/main/java/com/gssystems/flink/AggregatedTemperaturesTableAPI.java
+++ flink/FlinkETLTesting/src/main/java/com/gssystems/flink/AggregatedTemperaturesTableAPI.java
@@ -71,7 +71,7 @@ public class AggregatedTemperaturesTableAPI {
 		Table table1 = tableEnv.fromDataStream(stream);
 		System.out.println("Printing the table from stream...");
 		Expression selExpr = new SqlCallExpression(
-				"JSON_QUERY(f0, '$.latitude')"
+				"JSON_QUERY(f0, '$.lat')"
 		);
 		table1.select(selExpr).execute().print();
 		table1.printSchema();
